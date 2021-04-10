366d1528fec33ef9a65fd2d35e55ccd3de324c0d
Harsh Satish Singh <56669968+HarshSingh74@users.noreply.github.com>
2021-04-10 16:09:16 +0530

    Update gc_gui.py
    Improved_UI

--- gc_gui.py
+++ gc_gui.py
@@ -8,7 +8,7 @@ window.title("Gesture Control App")
 # Adjust size  
 
 window.geometry("980x680")
-window.configure(bg='white')
+
 
 # Add image file 
 
@@ -17,26 +17,21 @@ bg = PhotoImage( file = "features-1.png")
 
 label1 = Label( window, image = bg) 
 
-label1.place(x = 350,y = 250) 
+label1.place(x = 0,y = 0) 
 
 
 def callback(url):
     webbrowser.open_new(url)
 
 
-link1 = Label(window, text="Google Hyperlink", fg="blue",bg="white", cursor="hand2")
+link1 = Label(window, text="Click Here to Begin!", fg="blue",bg="white", cursor="hand2",font="Arial")
 link1.pack()
 link1.bind("<Button-1>", lambda e: callback("http://www.google.com"))
+link1.config(width=20)
 
-link2 = Label(window, text="Ecosia Hyperlink", fg="Black",bg="white", cursor="hand2")
-link2.pack()
-link2.bind("<Button-1>", lambda e: callback("http://www.ecosia.org"))
-
-label2 = Label( window, text = "Welcome to GesCon!",bg="white", font=("Arial", 25)).pack()
-label2.pack(pady = 50)
-
-label3 = Label( window, text = "We are a platform that provide Gesture Controlled Actions for your Faviourite Games!",bg="white", font=("Arial", 20)).pack()
-label3.pack(pady = 50)
+link1.place(relx = 0.73,
+                   rely = 0.5,
+                   anchor = 'center')
 
 
 
